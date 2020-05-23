1fde94393299e66ad53dd62926ee02c35c5a2a73
ericmugo1 <35067862+ericmugo1@users.noreply.github.com>
2020-05-24 00:05:05 +0200

    Updates

---
+++ precache-manifest.20925f38d6686a7571eebd416d7e10c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daeb2b5445520af891586e97bfb2488a",
+    "url": "/facebook-drop-down-menu/index.html"
+  },
+  {
+    "revision": "ba04b1490768a58c899c",
+    "url": "/facebook-drop-down-menu/static/css/main.67846ac5.chunk.css"
+  },
+  {
+    "revision": "0a04a972da957c19ba9d",
+    "url": "/facebook-drop-down-menu/static/js/2.3f5a30aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facebook-drop-down-menu/static/js/2.3f5a30aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba04b1490768a58c899c",
+    "url": "/facebook-drop-down-menu/static/js/main.68499536.chunk.js"
+  },
+  {
+    "revision": "82e00e3035b2b50546e2",
+    "url": "/facebook-drop-down-menu/static/js/runtime-main.68449b22.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/facebook-drop-down-menu/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/facebook-drop-down-menu/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/facebook-drop-down-menu/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/facebook-drop-down-menu/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/facebook-drop-down-menu/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/facebook-drop-down-menu/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/facebook-drop-down-menu/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/facebook-drop-down-menu/static/media/plus.9311af2e.svg"
+  }
+]);
